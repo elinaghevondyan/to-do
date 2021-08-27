0047578c20f1123235de8321e8a94189127ff32d
Elina Ghevondyan <elina@stdevmail.com>
2021-08-27 17:19:22 +0400

    Global style addition

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import '../../src/assets/scss/main.scss';
+
 import routeConfigs from "./routeConfigs";
 
 
